b432cdf1ccb9387a780a761ac59d060c0a98446a
Pavel Korolev <dev@borodust.org>
2018-01-28 21:21:33 +0300

    Add utility wrappers

--- glfw.lisp
+++ glfw.lisp
@@ -1,1 +1,69 @@
 (in-package :glfw)
+
+
+(defmacro define-window-close-callback (name (window) &body body)
+  `(claw:defcallback ,name :void ((,window (:pointer %glfw:window)))
+     ,@body))
+
+
+(defmacro define-key-callback (name (window key scancode action mod-keys) &body body)
+  `(claw:defcallback ,name :void ((,window (:pointer %glfw:window))
+                                  (,key :int)
+                                  (,scancode :int)
+                                  (,action :int)
+                                  (,mod-keys :int))
+     ,@body))
+
+
+(defmacro define-mouse-button-callback (name (window button action mod-keys) &body body)
+  `(claw:defcallback ,name :void ((,window (:pointer %glfw:window))
+                                  (,button :int)
+                                  (,action :int)
+                                  (,mod-keys :int))
+     ,@body))
+
+
+(defmacro define-cursor-pos-callback (name (window x y) &body body)
+  `(claw:defcallback ,name :void ((,window (:pointer %glfw:window))
+                                  (,x :double)
+                                  (,y :double))
+     ,@body))
+
+
+(defmacro define-scroll-callback (name (window x y) &body body)
+  `(claw:defcallback ,name :void ((,window (:pointer %glfw:window))
+                                  (,x :double)
+                                  (,y :double))
+     ,@body))
+
+
+(defmacro define-framebuffer-size-callback (name (window w h) &body body)
+  `(claw:defcallback ,name :void ((,window (:pointer %glfw:window))
+                                  (,w :int)
+                                  (,h :int))
+     ,@body))
+
+
+(defmacro define-char-callback (name (window codepoint) &body body)
+  `(claw:defcallback ,name :void ((,window (:pointer %glfw:window))
+                                  (,codepoint :unsigned-int))
+     ,@body))
+
+
+(defmacro with-window-hints ((&rest hints) &body body)
+  `(unwind-protect
+        (progn
+          ,@(loop for (hint value) in hints
+                  collect `(%glfw:window-hint ,hint ,value))
+          ,@body)
+     (%glfw:default-window-hints)))
+
+
+(defmacro with-init (() &body body)
+  (alexandria:with-gensyms (init-code)
+    `(let ((,init-code (%glfw:init)))
+       (unless (= ,init-code %glfw:+true+)
+         (error "Failed to initialize GLFW"))
+       (unwind-protect
+            (progn ,@body)
+         (%glfw:terminate)))))
--- packages.lisp
+++ packages.lisp
@@ -3,4 +3,13 @@
 
 (cl:defpackage :glfw
   (:use :cl :claw :alexandria)
-  (:export libglfw))
+  (:export libglfw
+           define-key-callback
+           define-window-close-callback
+           define-mouse-button-callback
+           define-cursor-pos-callback
+           define-scroll-callback
+           define-framebuffer-size-callback
+           define-char-callback
+           with-window-hints
+           with-init))
