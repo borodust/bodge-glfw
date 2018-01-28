8824f2170ad2731d032899e33c405b6108ca352d
Pavel Korolev <dev@borodust.org>
2018-01-28 17:22:01 +0300

    Fix GL/gl exclusion rule

--- claw.lisp
+++ claw.lisp
@@ -1,7 +1,7 @@
 (claw:c-include "bodge_glfw.h" bodge-glfw
   :in-package :%glfw
   :include-definitions ("(glfw|GLFW)\\w*")
-  :exclude-definitions ("(GL|gl)(?!(FW|fw))\\w*"
+  :exclude-definitions ("^(GL|gl)(?!(FW|fw))\\w*"
                         "^PFN\\w*"
                         "glfwGetProcAddress")
   :rename-symbols (claw:by-removing-prefixes "glfw" "GLFW" "GLFW_"))
