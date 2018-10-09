c54dc90f16004d46932df5bac206b496434af276
Pavel Korolev <dev@borodust.org>
2018-10-09 19:35:33 +0300

    Remove local-only

--- claw.lisp
+++ claw.lisp
@@ -1,6 +1,5 @@
 (claw:c-include "bodge_glfw.h" bodge-glfw
   :in-package :%glfw
-  :local-only t
   :sysincludes (:glfw-includes)
   :include-definitions ("(glfw|GLFW)\\w*")
   :exclude-definitions ("^(GL|gl)(?!(FW|fw))\\w*"
