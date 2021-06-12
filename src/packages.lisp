13039de90e2be1be52bae1bab3249fad158d0475
Pavel Korolev <dev@borodust.org>
2021-06-13 01:18:50 +0300

    Remove :claw from used packages

--- src/packages.lisp
+++ src/packages.lisp
@@ -1,13 +1,13 @@
 (cl:defpackage :glfw
-  (:use :cl :claw :alexandria)
-  (:export define-key-callback
-           define-window-close-callback
-           define-mouse-button-callback
-           define-cursor-pos-callback
-           define-scroll-callback
-           define-framebuffer-size-callback
-           define-window-size-callback
-           define-char-callback
-           define-joystick-callback
-           with-window-hints
-           with-init))
+  (:use :cl :alexandria)
+  (:export #:define-key-callback
+           #:define-window-close-callback
+           #:define-mouse-button-callback
+           #:define-cursor-pos-callback
+           #:define-scroll-callback
+           #:define-framebuffer-size-callback
+           #:define-window-size-callback
+           #:define-char-callback
+           #:define-joystick-callback
+           #:with-window-hints
+           #:with-init))
